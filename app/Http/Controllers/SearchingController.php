4527d718fe910fd263541f15f2273dc9d9428628
Amr Akram <75692992+proamrakram@users.noreply.github.com>
2022-07-11 04:34:39 +0200

    Update SearchingController.php

--- app/Http/Controllers/SearchingController.php
+++ app/Http/Controllers/SearchingController.php
@@ -9,21 +9,13 @@ class SearchingController extends Controller
 {
     public function searchProducts()
     {
-        // $categories = Category::all();
         $products = Product::with('category')->filter(request()->all())->paginate(10);
-
-        return response()->json([
-            'products' => $products
-        ]);
-        // return view('products-management.index', compact('products', 'categories'));
+        return response()->json([ 'products' => $products]);
     }
 
     public function searchTrashedProducts()
     {
         $products = Product::with('category')->onlyTrashed()->filter(request()->all())->paginate(10);
-        return response()->json([
-            'products' => $products
-        ]);
-        // return view('products-management.trash', compact('products'));
+        return response()->json([ 'products' => $products ]);
     }
 }
